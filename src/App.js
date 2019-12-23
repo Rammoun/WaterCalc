c355b97dc4674b94b12ab08f878be1d92b7c8398
Rammoun <44053646+Rammoun@users.noreply.github.com>
2019-12-23 10:59:41 +0100

    Water Calculater React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import DivBg from './DivBg/DivBg'
+import logo from './logo.svg';
+//cd share
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DivBg/>
+      </div>
+    );
+  }
+}
+
+export default App;
