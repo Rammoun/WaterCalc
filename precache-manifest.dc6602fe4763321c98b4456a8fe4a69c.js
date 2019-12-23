6defa9304a6dae7f4ed548112b45b4e5c457c546
Rammoun <44053646+Rammoun@users.noreply.github.com>
2019-12-23 10:52:37 +0100

    Updates

---
+++ precache-manifest.dc6602fe4763321c98b4456a8fe4a69c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rammoun/WaterCalc/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "740384b74c5ab98147e0",
+    "url": "/Rammoun/WaterCalc/static/js/runtime~main.740384b7.js"
+  },
+  {
+    "revision": "4c07dbd9f8b5d209579a",
+    "url": "/Rammoun/WaterCalc/static/js/main.4c07dbd9.chunk.js"
+  },
+  {
+    "revision": "895d46ef1d78098c4941",
+    "url": "/Rammoun/WaterCalc/static/js/1.895d46ef.chunk.js"
+  },
+  {
+    "revision": "4c07dbd9f8b5d209579a",
+    "url": "/Rammoun/WaterCalc/static/css/main.318714d7.chunk.css"
+  },
+  {
+    "revision": "895d46ef1d78098c4941",
+    "url": "/Rammoun/WaterCalc/static/css/1.017dab86.chunk.css"
+  },
+  {
+    "revision": "7d9dfc2c8b3a0f47b74b76f5b78b442c",
+    "url": "/Rammoun/WaterCalc/index.html"
+  }
+];
